b27874d23b054963b5777c32c7469dbfea66f949
susmab <susmab>
2005-02-26 14:10:46 +0000

    creation de la classe formula (vous enflammez pas, elle est vide pour le moment :p)

---
+++ dupratcated/src/fr/umlv/symphonie/data/formula/Formula.java
@@ -0,0 +1,14 @@
+/*
+ * This file is part of Symphonie
+ * Created : 26-févr.-2005 15:15:28
+ */
+package fr.umlv.symphonie.data.formula;
+
+
+/**
+ * @author susmab
+ *
+ */
+public class Formula {
+
+}
