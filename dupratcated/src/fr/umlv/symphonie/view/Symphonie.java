51ff34ceba51c2805691dd25c111ec9429d8cbaa
skimo <skimo>
2005-03-08 17:15:05 +0000

    view avec tab student et teacher

--- dupratcated/src/fr/umlv/symphonie/view/Symphonie.java
+++ dupratcated/src/fr/umlv/symphonie/view/Symphonie.java
@@ -9,9 +9,11 @@ import java.awt.Font;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 import javax.swing.Action;
 import javax.swing.ButtonGroup;
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JCheckBoxMenuItem;
 import javax.swing.JFrame;
@@ -21,33 +23,43 @@ import javax.swing.JMenuBar;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSeparator;
+import javax.swing.JSplitPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTable;
 import javax.swing.JToolBar;
+import javax.swing.JTree;
 import javax.swing.SwingConstants;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
 import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.tree.DefaultTreeCellRenderer;
 
+import fr.umlv.symphonie.data.Course;
 import fr.umlv.symphonie.data.DataManager;
+import fr.umlv.symphonie.data.DataManagerException;
 import fr.umlv.symphonie.data.SQLDataManager;
 import fr.umlv.symphonie.data.Student;
+import fr.umlv.symphonie.model.CourseTreeModel;
 import fr.umlv.symphonie.model.StudentModel;
+import fr.umlv.symphonie.model.StudentTreeModel;
+import fr.umlv.symphonie.model.TeacherModel;
 import fr.umlv.symphonie.util.SymphonieComponentBuilder;
 import fr.umlv.symphonie.util.TextualResourcesLoader;
+import fr.umlv.symphonie.view.cells.CellRendererFactory;
 
 public class Symphonie {
 
   private static final String FILE_PATTERN = "language/symphonie";
   private static final String FILE_CHARSET = "ISO-8859-1";
 
-  // C'est mal
   private static SymphonieComponentBuilder builder;
   private static HashMap<String, String> english;
   private static HashMap<String, String> french;
   private static JFrame frame;
 
-  private static JFrame getFrame() {
+  private static JFrame getFrame() throws DataManagerException {
 
     try {
       english = TextualResourcesLoader.getResourceMap(FILE_PATTERN, new Locale(
@@ -139,12 +151,12 @@ public class Symphonie {
         builder);
 
     // Items
-  
+
     JCheckBoxMenuItem frBox = (JCheckBoxMenuItem) builder.buildMenuItem(
         inFrench, SymphonieConstants.FRENCH_MENU_ITEM,
         SymphonieComponentBuilder.JMenuItemType.CHECK_BOX_ITEM);
     frBox.setSelected(true);
-    
+
     JCheckBoxMenuItem engBox = (JCheckBoxMenuItem) builder.buildMenuItem(
         inEnglish, SymphonieConstants.ENGLISH_MENU_ITEM,
         SymphonieComponentBuilder.JMenuItemType.CHECK_BOX_ITEM);
@@ -157,7 +169,7 @@ public class Symphonie {
     lang.add(frBox);
 
     lang.setIcon(new ImageIcon(Symphonie.class.getResource("icons/lang.png")));
-    
+
     return lang;
   }
 
@@ -226,9 +238,10 @@ public class Symphonie {
     JMenu format = builder.buildMenu(SymphonieConstants.FORMAT_MENU);
 
     /* Actions ******************************************************* */
-    Action formula = SymphonieActionFactory.getFormulaAction(new ImageIcon(Symphonie.class.getResource("icons/formula.png")), frame, builder);
-    Action f_cell = SymphonieActionFactory
-        .getFormulaCellAction(new ImageIcon(), frame, builder);
+    Action formula = SymphonieActionFactory.getFormulaAction(new ImageIcon(
+        Symphonie.class.getResource("icons/formula.png")), frame, builder);
+    Action f_cell = SymphonieActionFactory.getFormulaCellAction(
+        new ImageIcon(), frame, builder);
 
     /* Items********************************************************* */
     format.add(builder.buildMenuItem(formula,
@@ -248,8 +261,10 @@ public class Symphonie {
   private static JMenu getInsertMenu() {
     JMenu insert = builder.buildMenu(SymphonieConstants.INSERT_MENU);
     /* Actions ******************************************************* */
-    Action column = SymphonieActionFactory.getColumnAction(new ImageIcon(Symphonie.class.getResource("icons/insert_column.png")));
-    Action line = SymphonieActionFactory.getLineAction(new ImageIcon(Symphonie.class.getResource("icons/insert_line.png")));
+    Action column = SymphonieActionFactory.getColumnAction(new ImageIcon(
+        Symphonie.class.getResource("icons/insert_column.png")));
+    Action line = SymphonieActionFactory.getLineAction(new ImageIcon(
+        Symphonie.class.getResource("icons/insert_line.png")));
 
     /* Items********************************************************* */
     insert.add(builder.buildMenuItem(column,
@@ -270,7 +285,7 @@ public class Symphonie {
     JMenu admin = builder.buildMenu(SymphonieConstants.ADMIN_MENU);
     /* Actions ******************************************************* */
     Action connect = SymphonieActionFactory.getConnectAction(new ImageIcon(
-        Symphonie.class.getResource("icons/admin.png")),builder);
+        Symphonie.class.getResource("icons/admin.png")), builder);
     Action db = SymphonieActionFactory.getDBAction(new ImageIcon(
         Symphonie.class.getResource("icons/db.png")));
     Action pwd = SymphonieActionFactory.getPwdAction(new ImageIcon(
@@ -295,7 +310,7 @@ public class Symphonie {
   private static JToolBar getToolbar() {
     JToolBar toolbar = new JToolBar();
     toolbar.add(SymphonieActionFactory.getConnectAction(new ImageIcon(
-        Symphonie.class.getResource("icons/connect.png")),builder));
+        Symphonie.class.getResource("icons/connect.png")), builder));
     toolbar.add(SymphonieActionFactory.getDBAction(new ImageIcon(
         Symphonie.class.getResource("icons/db.png"))));
 
@@ -304,19 +319,102 @@ public class Symphonie {
     return toolbar;
   }
 
-  private static JPanel getContentPane(final JFrame f,
-      final SymphonieComponentBuilder builder) {
-    JPanel panel = new JPanel(new BorderLayout());
-    panel.add(getToolbar(), BorderLayout.NORTH);
+  private static JSplitPane getTeacherPane(DataManager dataManager) {
+    JSplitPane split = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
 
-    DataManager dataManager = new SQLDataManager();
-    StudentModel model = new StudentModel(dataManager);
+    /* table */
 
-    Student student = new Student(1, "Fabien", "Vallee");
+    final TeacherModel teacherModel = new TeacherModel(dataManager);
 
-    model.setStudent(student);
+    final JTable table = new JTable(teacherModel);
+    table.setTableHeader(null);
 
-    JTable table = new JTable(model);
+    table.setDefaultRenderer(Object.class, CellRendererFactory
+        .getTeacherModelCellRenderer(teacherModel.getFormattedObjects()));
+
+    JScrollPane scroll1 = new JScrollPane(table);
+
+    split.setRightComponent(scroll1);
+
+    /*
+     * arbre
+     */
+    CourseTreeModel courseModel = new CourseTreeModel(dataManager);
+
+    final JTree tree = new JTree(courseModel);
+
+    tree.setCellRenderer(new DefaultTreeCellRenderer() {
+
+      private final Icon leafIcon = new ImageIcon(StudentTreeModel.class
+          .getResource("../view/icons/course.png"));
+      private final Icon rootIcon = new ImageIcon(StudentTreeModel.class
+          .getResource("../view/icons/courses.png"));
+
+      public java.awt.Component getTreeCellRendererComponent(JTree tree,
+          Object value, boolean sel, boolean expanded, boolean leaf, int row,
+          boolean hasFocus) {
+
+        Font font = getFont();
+
+        if (font != null) {
+          font = font.deriveFont(Font.BOLD);
+          setFont(font);
+        }
+
+        if (leaf) {
+          setLeafIcon(leafIcon);
+        } else {
+          setClosedIcon(rootIcon);
+          setOpenIcon(rootIcon);
+        }
+
+        super.getTreeCellRendererComponent(tree, value, sel, expanded, leaf,
+            row, hasFocus);
+        return this;
+      }
+    });
+
+    tree.addTreeSelectionListener(new TreeSelectionListener() {
+
+      /*
+       * (non-Javadoc)
+       * 
+       * @see javax.swing.event.TreeSelectionListener#valueChanged(javax.swing.event.TreeSelectionEvent)
+       */
+      public void valueChanged(TreeSelectionEvent e) {
+        Object o = tree.getLastSelectedPathComponent();
+
+        if (o instanceof Course) {
+          System.out.println("on a selectionne une matiere !");
+          ((TeacherModel) table.getModel()).setCourse((Course) o);
+        }
+      }
+
+    });
+
+    JScrollPane pane = new JScrollPane(tree);
+
+    split.setLeftComponent(pane);
+
+    return split;
+
+  }
+
+  private static JSplitPane getStudentPane(DataManager dataManager)
+      throws DataManagerException {
+    StudentModel studentModel = new StudentModel(dataManager);
+
+    Map<Integer, Student> studentMap = null;
+
+    studentMap = dataManager.getStudents();
+
+    JSplitPane split = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+
+    /*
+     * table
+     */
+
+    final JTable table = new JTable(studentModel);
     table.setTableHeader(null);
 
     table.setDefaultRenderer(Object.class, new DefaultTableCellRenderer() {
@@ -336,11 +434,90 @@ public class Symphonie {
       }
     });
 
-    JScrollPane scroll = new JScrollPane(table);
+    JScrollPane scroll1 = new JScrollPane(table);
+    split.setRightComponent(scroll1);
+
+    /*
+     * arbre
+     */
+
+    StudentTreeModel treeModel = new StudentTreeModel(dataManager);
+    final JTree tree = new JTree(treeModel);
+
+    tree.setCellRenderer(new DefaultTreeCellRenderer() {
+
+      private final Icon leafIcon = new ImageIcon(StudentTreeModel.class
+          .getResource("../view/icons/student.png"));
+      private final Icon rootIcon = new ImageIcon(StudentTreeModel.class
+          .getResource("../view/icons/students.png"));
+
+      public java.awt.Component getTreeCellRendererComponent(JTree tree,
+          Object value, boolean sel, boolean expanded, boolean leaf, int row,
+          boolean hasFocus) {
+
+        Font font = getFont();
+
+        if (font != null) {
+          font = font.deriveFont(Font.BOLD);
+          setFont(font);
+        }
+
+        if (leaf) {
+          setLeafIcon(leafIcon);
+        } else {
+          setClosedIcon(rootIcon);
+          setOpenIcon(rootIcon);
+        }
+
+        super.getTreeCellRendererComponent(tree, value, sel, expanded, leaf,
+            row, hasFocus);
+        return this;
+      }
+    });
+
+    JScrollPane pane = new JScrollPane(tree);
+
+    split.setLeftComponent(pane);
+
+    tree.addTreeSelectionListener(new TreeSelectionListener() {
+
+      /*
+       * (non-Javadoc)
+       * 
+       * @see javax.swing.event.TreeSelectionListener#valueChanged(javax.swing.event.TreeSelectionEvent)
+       */
+      public void valueChanged(TreeSelectionEvent e) {
+        Object o = tree.getLastSelectedPathComponent();
+
+        if (o instanceof Student)
+          ((StudentModel) (table.getModel())).setStudent((Student) o);
+
+      }
+    });
+
+    return split;
+  }
+
+  private static JPanel getContentPane(final JFrame f,
+      final SymphonieComponentBuilder builder) throws DataManagerException {
+    JPanel panel = new JPanel(new BorderLayout());
+    panel.add(getToolbar(), BorderLayout.NORTH);
+
+    DataManager dataManager = new SQLDataManager();
+
+    
     JTabbedPane tab = new JTabbedPane(JTabbedPane.BOTTOM,
         JTabbedPane.SCROLL_TAB_LAYOUT);
-    tab.add(SymphonieConstants.VIEW_STUDENT_MENU_ITEM, scroll);
+    
+    /* Student JTabbedPane */
+    tab.add(SymphonieConstants.VIEW_STUDENT_MENU_ITEM, new JScrollPane(getStudentPane(dataManager)));
     panel.add(tab);
+    
+    /* Teacher JTabbedPane */
+
+    tab.add(SymphonieConstants.VIEW_TEACHER_MENU_ITEM, new JScrollPane(getTeacherPane(dataManager)));
+    panel.add(tab);
+    
     builder.addChangeListener(panel, new ChangeListener() {
 
       public void stateChanged(ChangeEvent arg0) {
@@ -351,7 +528,7 @@ public class Symphonie {
     return panel;
   }
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws DataManagerException {
     JFrame f = getFrame();
     f.pack();
     f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
