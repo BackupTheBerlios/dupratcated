c4bf0ce6c29a4c49552e1a55ba35bd70788f6226
snowman <snowman>
2005-03-09 16:47:28 +0000

    test

---
+++ dupratcated/test/src/fr/umlv/symphonie/util/dataimport/xml/XMLImporterTest.java
@@ -0,0 +1,22 @@
+/*
+ * This file is part of Symphonie
+ * Created : 8 mars 2005 21:52:16
+ */
+package fr.umlv.symphonie.util.dataimport.xml;
+
+import junit.framework.TestCase;
+import fr.umlv.symphonie.util.dataimport.DataImporter;
+import fr.umlv.symphonie.util.dataimportation.xml.XMLImporter;
+
+
+/**
+ * @author Laurent GARCIA
+ *
+ */
+public class XMLImporterTest extends TestCase {
+
+  DataImporter di = new XMLImporter();
+  public void testImport() {    
+    di.importStudentView("student_view.xml");   
+  }
+}
