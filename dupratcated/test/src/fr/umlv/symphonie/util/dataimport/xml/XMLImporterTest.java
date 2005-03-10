7092299dcbd70df8a81b61585dd05eaff72c7233
snowman <snowman>
2005-03-10 15:49:02 +0000

    ttt

--- dupratcated/test/src/fr/umlv/symphonie/util/dataimport/xml/XMLImporterTest.java
+++ dupratcated/test/src/fr/umlv/symphonie/util/dataimport/xml/XMLImporterTest.java
@@ -7,20 +7,37 @@ package fr.umlv.symphonie.util.dataimport.xml;
 import junit.framework.TestCase;
 import fr.umlv.symphonie.util.dataimport.DataImporter;
 import fr.umlv.symphonie.util.dataimport.DataImporterException;
-import fr.umlv.symphonie.util.dataimportation.xml.XMLImporter;
+import fr.umlv.symphonie.util.dataimport.xml.XMLImporter;
 
 /**
  * @author Laurent GARCIA
  */
 public class XMLImporterTest extends TestCase {
 
-  DataImporter di = new XMLImporter();
+	DataImporter di = new XMLImporter();
 
-  public void testImportStudentView() {
-    try {
-      di.importStudentView("student_view.xml");
-    } catch (DataImporterException e) {
-      e.printStackTrace();
-    }
-  }
+	public void testImportStudentView() {
+		try {
+			di.importStudentView("student_view.xml");
+		} catch (DataImporterException e) {
+			;
+		}
+	}
+
+	public void testImportTeacherView() {
+		try {
+			di.importTeacherView("teacher_view.xml");
+		} catch (DataImporterException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void testImportJuryView() {
+		try {
+			di.importTeacherView("jury_view.xml");
+		} catch (DataImporterException e) {
+			e.printStackTrace();
+		}
+	}
+
 }
