116dc69bc2a5ce7a4f372882694496648757f8b5
snowman <snowman>
2005-03-14 12:43:56 +0000

    un peu de commentaire et changeCourse()

--- dupratcated/src/fr/umlv/symphonie/data/DataManager.java
+++ dupratcated/src/fr/umlv/symphonie/data/DataManager.java
@@ -6,52 +6,211 @@ import java.util.SortedMap;
 
 import fr.umlv.symphonie.util.Pair;
 
+public interface DataManager {
+	/**	 * 
+	 * @return 
+	 * @throws DataManagerException
+	 */
+	public Map<Integer, Student> getStudents() throws DataManagerException;
 
-public interface DataManager
-{    
-	public Map<Integer, Student> getStudents()throws DataManagerException;
-  public List<Student> getStudentList() throws DataManagerException;
-	public Map<Integer, Course> getCourses()throws DataManagerException;
-  public List<Course> getCoursesList() throws DataManagerException;
-	public Map<Integer, Mark> getMarks()throws DataManagerException;
-  public List<StudentMark> getStudentMarks()throws DataManagerException;
-	public Map<Integer, Mark> getMarksByCourse(Course c)throws DataManagerException;
-	
-  /* methodes servant a la vue etudiant */
-	public Map<Integer, StudentMark> getMarksByStudentAndCourse(Student s, Course c)throws DataManagerException;
-  public Map<Course, Map<Integer, StudentMark>> getAllMarksByStudent(Student s)throws DataManagerException;
-  
-  
-  
-  /* methodes servant a la vue professeur */
-	public Pair<Map<Integer, Mark>,
-              SortedMap<Student, Map<Integer, StudentMark>>> getAllMarksByCourse(Course c)throws DataManagerException;
-	
-  /* methodes servant a la vue jury */
-	public Pair<Map<Integer, Course>, SortedMap<Student, Map<Course, Map<Integer, StudentMark>>>> getAllStudentsMarks()throws DataManagerException;
-	
+	/**
+	 * @return
+	 * @throws DataManagerException
+	 */
+	public List<Student> getStudentList() throws DataManagerException;
 
-  /* methodes pour l'admin */
-	public void addStudent(String name, String lastName) throws DataManagerException;
-	public void addStudents(List<Pair<String, String>> namesList) throws DataManagerException;
+	/**
+	 * @return
+	 * @throws DataManagerException
+	 */
+	public Map<Integer, Course> getCourses() throws DataManagerException;
+
+	/**
+	 * @return
+	 * @throws DataManagerException
+	 */
+	public List<Course> getCoursesList() throws DataManagerException;
+
+	/**
+	 * @return
+	 * @throws DataManagerException
+	 */
+	public Map<Integer, Mark> getMarks() throws DataManagerException;
+
+	/**
+	 * @return
+	 * @throws DataManagerException
+	 */
+	public List<StudentMark> getStudentMarks() throws DataManagerException;
+
+	/**
+	 * @param c
+	 * @return
+	 * @throws DataManagerException
+	 */
+	public Map<Integer, Mark> getMarksByCourse(Course c)
+			throws DataManagerException;
+
+	/**
+	 * for the student view
+	 * @param s
+	 * @param c
+	 * @return
+	 * @throws DataManagerException
+	 */
+	public Map<Integer, StudentMark> getMarksByStudentAndCourse(Student s,
+			Course c) throws DataManagerException;
+
+	/**
+	 * for the student view
+	 * @param s
+	 * @return
+	 * @throws DataManagerException
+	 */
+	public Map<Course, Map<Integer, StudentMark>> getAllMarksByStudent(Student s)
+			throws DataManagerException;
+
+	/**
+	 * for the teacher view
+	 * @param c
+	 * @return
+	 * @throws DataManagerException
+	 */
+	public Pair<Map<Integer, Mark>, SortedMap<Student, Map<Integer, StudentMark>>> getAllMarksByCourse(
+			Course c) throws DataManagerException;
+	
+	/**
+	 * for the jury view
+	 * @return
+	 * @throws DataManagerException
+	 */
+	public Pair<Map<Integer, Course>, SortedMap<Student, Map<Course, Map<Integer, StudentMark>>>> getAllStudentsMarks()
+			throws DataManagerException;
+
+	
+	/**
+	 * for the admin 
+	 * @param name
+	 * @param lastName
+	 * @throws DataManagerException
+	 */
+	public void addStudent(String name, String lastName)
+			throws DataManagerException;
+
+	/**
+	 * for the admin 
+	 * @param namesList
+	 * @throws DataManagerException
+	 */
+	public void addStudents(List<Pair<String, String>> namesList)
+			throws DataManagerException;
+
+	/**
+	 * for the admin 
+	 * @param s
+	 * @throws DataManagerException
+	 */
 	public void removeStudent(Student s) throws DataManagerException;
-	
-	public void addCourse(String title, float coeff) throws DataManagerException;
-	public void addCourses(List<Pair<String, Float>> courseList) throws DataManagerException;
+
+	/**
+	 * for the admin 
+	 * @param title
+	 * @param coeff
+	 * @throws DataManagerException
+	 */
+	public void addCourse(String title, float coeff)
+			throws DataManagerException;
+
+	/**
+	 * for the admin 
+	 * @param courseList
+	 * @throws DataManagerException
+	 */
+	public void addCourses(List<Pair<String, Float>> courseList)
+			throws DataManagerException;
+
+	/**
+	 * for the admin 
+	 * @param c
+	 * @throws DataManagerException
+	 */
 	public void removeCourse(Course c) throws DataManagerException;
 
-  public int addTitle(String desc) throws DataManagerException;
-  
-	public void addMark(String desc, float coeff, Course c) throws DataManagerException;
+	/**
+	 * for the admin 
+	 * @param desc
+	 * @throws DataManagerException
+	 */
+	public int addTitle(String desc) throws DataManagerException;
+
+	/**
+	 * for the admin 
+	 * @param desc
+	 * @param coeff
+	 * @param c
+	 * @throws DataManagerException
+	 */
+	public void addMark(String desc, float coeff, Course c)
+			throws DataManagerException;
+
+	/**
+	 * for the admin 
+	 * @param m
+	 * @throws DataManagerException
+	 */
 	public void removeMark(Mark m) throws DataManagerException;
-  
-  
-  /* methodes d'edition */
-  public void changeStudentComment(Student s, String newComment)throws DataManagerException;
-  
-  public void changeStudentMarkValue(StudentMark studentMark, float newValue) throws DataManagerException;
-  
-  public void changeMarkCoeff(Mark mark, float newCoeff) throws DataManagerException;
-  public void changeMarkDescription(Mark mark, String newDescription) throws DataManagerException;
-  
+
+	/**
+	 * to edit 
+	 * @param s
+	 * @param newComment
+	 * @throws DataManagerException
+	 */
+	public void changeStudentComment(Student s, String newComment)
+			throws DataManagerException;
+
+	/**
+	 * to edit 
+	 * @param sm
+	 * @param newValue
+	 * @throws DataManagerException
+	 */
+	public void changeStudentMarkValue(StudentMark sm, float newValue)
+			throws DataManagerException;
+
+	/**
+	 * to edit 
+	 * @param m
+	 * @param newCoeff
+	 * @throws DataManagerException
+	 */
+	public void changeMarkCoeff(Mark m, float newCoeff)
+			throws DataManagerException;
+
+	/**
+	 * to edit 
+	 * @param m
+	 * @param newDescription
+	 * @throws DataManagerException
+	 */
+	public void changeMarkDescription(Mark m, String newDescription)
+			throws DataManagerException;
+
+	/**
+	 * to edit 
+	 * @param c
+	 * @param newTitle
+	 * @throws DataManagerException
+	 */
+	public void changeCourseTitle(Course c, String newTitle)
+			throws DataManagerException;
+
+	/**
+	 * to edit 
+	 * @param c
+	 * @param newCoeff
+	 * @throws DataManagerException
+	 */
+	public void changeCourseCoeff(Course c, float newCoeff)
+			throws DataManagerException;
 }
