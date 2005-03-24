50f89d1dc6b8265c00cc7816a7e5c6606f24aee6
spraynasal <spraynasal>
2005-03-24 14:26:41 +0000

    Truc à deux balles

---
+++ dupratcated/src/fr/umlv/symphonie/util/LookableCollection.java
@@ -0,0 +1,24 @@
+/*
+ * This file is part of Symphonie
+ * Created on 24 mars 2005
+ */
+package fr.umlv.symphonie.util;
+
+import java.util.Collection;
+
+/**
+ * Interface defines a collection where one can easily search for objects
+ * 
+ * @author spenasal
+ */
+public interface LookableCollection<T> extends Collection<T> {
+
+	/**
+	 * Perform a lookup and returns the closest matching object to the passed
+	 * one.
+	 * 
+	 * @param t
+	 *            The object to use as the base for the lookup.
+	 */
+	public T lookup(T t);
+}
