a4db3d55324338f2c306181b83f5e2b12f1365ad
spraynasal <spraynasal>
2005-03-28 08:11:01 +0000

    Les JUNIT couille ça suxx

--- dupratcated/test/src/fr/umlv/symphonie/util/TestSymphoniePreferencesManager.java
+++ dupratcated/test/src/fr/umlv/symphonie/util/TestSymphoniePreferencesManager.java
@@ -2,6 +2,7 @@
  * This file is part of Symphonie
  * Created : 20 mars 2005 17:00:44
  */
+
 package fr.umlv.symphonie.util;
 
 import java.util.Properties;
@@ -11,20 +12,26 @@ import static fr.umlv.symphonie.util.SymphoniePreferencesManager.*;
 import fr.umlv.symphonie.util.SymphoniePreferencesManager.DataBaseType;
 import fr.umlv.symphonie.view.Symphonie;
 
-
 /**
  * Preferences test cases
+ * 
  * @author PEÑA SALDARRIAGA Sébastian
  */
 public class TestSymphoniePreferencesManager extends TestCase {
 
+  protected void setUp() throws Exception {
+    super.setUp();
+    original = getDBProperties();
+    lang = getLanguage();
+    db = getDBType();
+  }
+  
   public void testSetLanguage() {
     setLanguage(Symphonie.Language.SPANISH);
   }
 
   public void testGetLanguage() {
     Symphonie.Language l = getLanguage();
-    System.out.println("Stored language : " + l);
     assertEquals(l, Symphonie.Language.SPANISH);
   }
 
@@ -34,7 +41,6 @@ public class TestSymphoniePreferencesManager extends TestCase {
 
   public void testGetDBType() {
     DataBaseType dbt = getDBType();
-    System.out.println("Stored db type : " + dbt);
     assertEquals(dbt, DataBaseType.PostgreSQL);
   }
 
@@ -50,11 +56,25 @@ public class TestSymphoniePreferencesManager extends TestCase {
   public void testGetDBProperties() {
     String expectedURL = "jdbc:postgresql://localhost/symphonie";
     Properties props = getDBProperties();
-    System.out.println("Connection properties " + props);
+    assertNotNull(props);
     assertEquals(expectedURL, props.getProperty("url"));
   }
-  
+
   public void testClearPreferences() {
     clearPreferences();
+  }
+
+  private Properties original;
+  private Symphonie.Language lang;
+  private DataBaseType db;
+
+  protected void tearDown() throws Exception {
+    super.tearDown();
+    if (original != null)
+      setDBProperties(original);
+    else
+      clearPreferences();
+    if (db != null) setDBType(db);
+    setLanguage(lang);
   }
 }
