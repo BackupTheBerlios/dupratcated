a78de304f3a4bd4e39690d8c333cf302b565447a
snowman <snowman>
2005-03-28 14:40:21 +0000

    comment

--- dupratcated/src/fr/umlv/symphonie/util/dataimport/DataImporter.java
+++ dupratcated/src/fr/umlv/symphonie/util/dataimport/DataImporter.java
@@ -23,7 +23,8 @@ public interface DataImporter {
 	 *            the DataManager with which we will set or create the data.
 	 * @throws DataImporterException
 	 *             if the xml file doesn't respect the dtd, or if there is a
-	 *             problem with the bdd.
+	 *             problem with the bdd, or if the xml file doesn't exist, or if
+	 *             the xml file isn't in the correct view.
 	 */
 	public void importStudentView(String documentName, DataManager dm)
 			throws DataImporterException;
@@ -40,15 +41,16 @@ public interface DataImporter {
 	 *            the DataManager with which we will set or create the data.
 	 * @throws DataImporterException
 	 *             if the xml file doesn't respect the dtd, or if there is a
-	 *             problem with the bdd.
+	 *             problem with the bdd, or if the xml file doesn't exist, or if
+	 *             the xml file isn't in the correct view.
 	 */
 	public void importTeacherView(String documentName, DataManager dm)
 			throws DataImporterException;
 
 	/**
 	 * import a jury view with the datas which are in the file documentName, and
-	 * set or create the data in the bdd with the DataManager dm. If an id
-	 * of a node has the value -1, the datas will be created, else they will be
+	 * set or create the data in the bdd with the DataManager dm. If an id of a
+	 * node has the value -1, the datas will be created, else they will be
 	 * updated.
 	 * 
 	 * @param documentName
@@ -57,7 +59,8 @@ public interface DataImporter {
 	 *            the DataManager with which we will set or create the data.
 	 * @throws DataImporterException
 	 *             if the xml file doesn't respect the dtd, or if there is a
-	 *             problem with the bdd.
+	 *             problem with the bdd, or if the xml file doesn't exist, or if
+	 *             the xml file isn't in the correct view.
 	 */
 	public void importJuryView(String documentName, DataManager dm)
 			throws DataImporterException;
