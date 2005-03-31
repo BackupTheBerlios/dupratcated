77a6d5e39eb91de8d1828c2aa6c505adb89a8e1f
susmab <susmab>
2005-03-31 01:39:44 +0000

    *** empty log message ***

--- dupratcated/test/src/fr/umlv/symphonie/data/SQLDataManagerTest.java
+++ dupratcated/test/src/fr/umlv/symphonie/data/SQLDataManagerTest.java
@@ -4,10 +4,17 @@
  */
 package fr.umlv.symphonie.data;
 
+import static fr.umlv.symphonie.data.SQLDataManagerConstants.TABLE_COURSE;
+import static fr.umlv.symphonie.data.SQLDataManagerConstants.TABLE_HAS_MARK;
+import static fr.umlv.symphonie.data.SQLDataManagerConstants.TABLE_STUDENT;
+import static fr.umlv.symphonie.data.SQLDataManagerConstants.TABLE_TEST;
+
 import java.sql.SQLException;
-import java.util.Map;
+import java.util.ArrayList;
 
 import junit.framework.TestCase;
+
+
 
 
 /**
@@ -16,208 +23,246 @@ import junit.framework.TestCase;
  */
 public class SQLDataManagerTest extends TestCase {
 
-  SQLDataManager dm = SQLDataManager.getInstance();
+  private final static SQLDataManager manager = SQLDataManager.getInstance();
   
-  public void testCreatePrimaryKey() throws SQLException, DataManagerException {
-    int key1 = -1;
-    int key2 = -1;
-    Student t = null;
-
-    key1 = SQLDataManager.createPrimaryKey("student", "id_student");
-    t = new Student(key1, "Laurent", "Baka");
-    dm.addStudent(t.getName(), t.getLastName());
-    key2 = SQLDataManager.createPrimaryKey("student", "id_student");
-    dm.removeStudent(t);
-
-    assertEquals(key1, (key2 - 1));
-  }
+  private static Student student = null;
+  private static Course course = null;
+  private static Mark mark = null;
   
-  public void testAddStudent() throws SQLException, DataManagerException {
-    int count = 1;
-    Student t1 = null;
-    Map<Integer, Student> all = dm.getStudents();
-   
-    t1 = new Student(SQLDataManager.createPrimaryKey("student", "id_student"), "Laurent",
-        "Baka");
-    dm.addStudent(t1.getName(), t1.getLastName());
   
-    for (Student t : all.values()) {    
-      if (t.getId() == t1.getId() 
-          && t.getName().equals(t1.getName())
-          && t.getLastName().equals(t1.getLastName())) {
-        dm.removeStudent(t1);
-        count--;
-      }
-    }
-
-    assertEquals(0, count);
-  }
-
-  public void testAddStudents() throws SQLException, DataManagerException {
-    /*int count = 2;
-    int id = -1;
-    Student t1 = null;
-    Student t2 = null;
-    Map<Integer, Student> all = dm.getStudents();
-    ArrayList<String> listName = new ArrayList<String>();
-    ArrayList<String> listLastName = new ArrayList<String>();
-
-    id = SQLDataManager.createPrimaryKey("student", "id_student");
-    t1 = new Student(id, "Laurent", "Baka");
-    t2 = new Student(id + 1, "Fabien", "Kyubi");
-
-    listName.add(t1.getName());
-    listName.add(t2.getName());
-
-    listLastName.add(t1.getLastName());
-    listLastName.add(t2.getLastName());
-
-    dm.addStudents(listName, listLastName);    
-
-    for (Student t : all.values()) {
-      if (t.getId() == t1.getId()
-          && t.getName().equals(t1.getName())
-          && t.getLastName().equals(t1.getLastName())) {
-        dm.removeStudent(t1);
-        count--;
-      } else if (t.getId() == t2.getId()
-          && t.getName().equals(t2.getName())
-          && t.getLastName().equals(t2.getLastName())) {
-        dm.removeStudent(t2);
-        count--;
-      }
-    }
-
-    assertEquals(0, count);*/
-  }
-
-  public void testRemoveStudent() throws SQLException, DataManagerException {
-    Student t1 = null;
-    Map<Integer, Student> all = dm.getStudents();
-
-    t1 = new Student(SQLDataManager.createPrimaryKey("student", "id_student"), "Laurent",
-        "Baka");
-    dm.addStudent(t1.getName(), t1.getLastName());
-    dm.removeStudent(t1);
-
-    for (Student t : all.values()) {
-      assertFalse(t.getId() == t1.getId());
-    }
-  }
   
-  public void testRemoveAllStudentMarksForStudent() {
+  
+  public void testGetStudentList() throws SQLException, DataManagerException {
+    int n = manager.sizeOfTable(TABLE_STUDENT);
+    
+    assertEquals(n, manager.getStudentList().size());
   }
+
+  public void testGetCoursesList() throws SQLException, DataManagerException {
+    int n = manager.sizeOfTable(TABLE_COURSE);
+    
+    assertEquals(n, manager.getCoursesList().size());
+  }
+
+  public void testGetMarks() throws SQLException, DataManagerException {
+    int n = manager.sizeOfTable(TABLE_TEST);
+    
+    assertEquals(n, manager.getMarks().size());
+  }
+
+  public void testGetStudentMarks() throws SQLException, DataManagerException {
+    int n = manager.sizeOfTable(TABLE_HAS_MARK);
+    
+    assertEquals(n, manager.getStudentMarks().size());
+  }
+
+  public void testAddStudent() throws DataManagerException, SQLException {
+    
+    int studentCount = manager.sizeOfTable(TABLE_STUDENT);
+    int testsCount = manager.sizeOfTable(TABLE_TEST);
+    int studentMarksCount = manager.sizeOfTable(TABLE_HAS_MARK);
+    
+    student = manager.addStudent("test", "test");
+    
+    assertEquals(studentCount + 1, manager.sizeOfTable(TABLE_STUDENT));
+    assertEquals(studentMarksCount + testsCount, manager.sizeOfTable(TABLE_HAS_MARK));
+  }
+
+
 
   public void testAddCourse() throws SQLException, DataManagerException {
-    int count = 1;
-    Course t1 = null;
-    Map<Integer, Course> all = dm.getCourses();
-
-    t1 = new Course(SQLDataManager.createPrimaryKey("course", "id_course"), "Java",
-        (float) 6.0);
-
-    dm.addCourse(t1.getTitle(), t1.getCoeff());
-
-    for (Course t : all.values()) {
-      if (t.getId() == t1.getId()
-          && t.getTitle().equals(t1.getTitle())
-          && t.getCoeff() == t1.getCoeff()) {
-        dm.removeCourse(t1);
-        count--;
-      }
-    }
-
-    assertEquals(0, count);
+    int n = manager.sizeOfTable(TABLE_COURSE);
+    
+    course = manager.addCourse("testCourse", 0f);
+    
+    assertEquals(n + 1, manager.sizeOfTable(TABLE_COURSE));
   }
 
-  public void testAddCourses() throws SQLException, DataManagerException {
-   /* int count = 2;
-    int id = -1;
-    Course t1 = null;
-    Course t2 = null;
-    Map<Integer, Course> all = dm.getCourses();
-    ArrayList<String> listTitle = new ArrayList<String>();
-    ArrayList<Float> listCoeff = new ArrayList<Float>();
 
-    id = SQLDataManager.createPrimaryKey("student", "id_student");
-    t1 = new Course(id, "Java", (float) 6.0);
-    t2 = new Course(id + 1, "C++", (float) 6.0);
 
-    listTitle.add(t1.getTitle());
-    listTitle.add(t2.getTitle());
-
-    listCoeff.add(t1.getCoeff());
-    listCoeff.add(t2.getCoeff());
-
-    dm.addCourses(listTitle, listCoeff);
-
-    for (Course t : all.values()) {
-      if (t.getId() == t1.getId() 
-          && t.getTitle().equals(t1.getTitle())
-          && t.getCoeff() == t1.getCoeff()) {
-        dm.removeCourse(t1);
-        count--;
-      } else if (t.getId() == t2.getId()
-          && t.getTitle().equals(t2.getTitle())
-          && t.getCoeff() == t2.getCoeff()) {
-        dm.removeCourse(t2);
-        count--;
-      }
-    }
-
-    assertEquals(0, count);*/
-  }
-
-  public void testRemoveCourse() throws SQLException, DataManagerException {
-    Course t1 = null;
-    Map<Integer, Course> all = dm.getCourses();
-
-    t1 = new Course(SQLDataManager.createPrimaryKey("course", "id_course"), "Java",
-        (float) 6.0);
-    dm.addCourse(t1.getTitle(), t1.getCoeff());
-    dm.removeCourse(t1);
-
-    for (Course t : all.values()) {
-      assertFalse(t.getId() == t1.getId());
+  public void testAddMark() throws SQLException, DataManagerException {
+    int testsCount = manager.sizeOfTable(TABLE_TEST);
+    int studentMarksCount = manager.sizeOfTable(TABLE_HAS_MARK);
+    int studentCount = manager.sizeOfTable(TABLE_STUDENT);
+    
+    if (course != null){
+    mark = manager.addMark("testMark", 0f, course);
+    
+    assertEquals(testsCount + 1, manager.sizeOfTable(TABLE_TEST));
+    assertEquals(studentMarksCount + studentCount, manager.sizeOfTable(TABLE_HAS_MARK));
+    
     }
   }
 
-  public void testAddTitle() throws DataManagerException, SQLException {
-    int count = 1;
-    Course c1 = null;
-    Mark t1 = null;
-    Map<Integer, Mark> all = dm.getMarksByCourse(c1);
 
-    c1 = new Course(SQLDataManager.createPrimaryKey("course", "id_course"), "Java",
-        (float) 6.0);
-    t1 = new Mark(SQLDataManager.createPrimaryKey("title", "id_title"), "Project", (float)0.5, c1);
 
-    dm.addTitle(t1.getDesc());
-
-    for (Mark t : all.values()) {
-      if (t.getId() == t1.getId()
-          && t.getDesc().equals(t1.getDesc())) {
-        dm.removeMark(t1);
-        count--;
-      }
+  public void testChangeStudentMarkValue() throws DataManagerException {
+    StudentMark sm;
+    
+    if (student != null && course != null) {
+      sm = new ArrayList<StudentMark>(manager.getMarksByStudentAndCourse(
+          student, course).values()).get(0);
+      float oldValue = sm.getValue();
+      
+      manager.changeStudentMarkValue(sm,oldValue + 99);
+      assertEquals(oldValue + 99, sm.getValue());
     }
-
-    assertEquals(0, count);
   }
 
-  public void testAddMark() {
+  public void testChangeMarkDescriptionAndCoeff() throws DataManagerException {
+    if (mark != null){
+      String newTitle = "testNewTitle";
+      float newCoeff = 99f;
+      
+      manager.changeMarkDescriptionAndCoeff(mark, newTitle, newCoeff);
+      
+      assertEquals(newTitle, mark.getDesc());
+      assertEquals(true, newCoeff == mark.getCoeff());
+    }
   }
 
-  public void testRemoveMark() {
+  public void testChangeMarkDescription() throws DataManagerException {
+    if (mark != null){
+      String newTitle = "testNewTitle2";
+      
+      manager.changeMarkDescription(mark, newTitle);
+      
+      assertEquals(newTitle, mark.getDesc());
+    }
+  }
+
+  public void testChangeMarkCoeff() throws DataManagerException {
+    if (mark != null){
+      float newCoeff = 999f;
+      
+      manager.changeMarkCoeff(mark, newCoeff);
+      
+      assertEquals(true, newCoeff == mark.getCoeff());
+    }
+  }
+
+  public void testChangeStudentNameAndLastNameAndComment() throws DataManagerException {
+    if (student != null){
+      String newName = "newName";
+      String newLastName = "newLastName";
+      String newComment = "newComment";
+      
+      manager.changeStudentNameAndLastNameAndComment(student, newName, newLastName, newComment);
+      
+      assertEquals(newName, student.getName());
+      assertEquals(newLastName, student.getLastName());
+      assertEquals(newComment, student.getComment());
+    }
+  }
+
+  public void testChangeStudentComment() throws DataManagerException {
+    if (student != null){
+      String newComment = "newComment2";
+      
+      manager.changeStudentComment(student, newComment);
+      
+      assertEquals(newComment, student.getComment());
+    }
+  }
+
+  public void testChangeStudentName() throws DataManagerException {
+    if (student != null){
+      String newName = "newName2";
+      
+      manager.changeStudentName(student, newName);
+      
+      assertEquals(newName, student.getName());
+    }
+  }
+
+  public void testChangeStudentLastName() throws DataManagerException {
+    if (student != null){
+      String newLastName = "newLastName2";
+      
+      manager.changeStudentLastName(student, newLastName);
+      
+      assertEquals(newLastName, student.getLastName());
+    }
+  }
+
+  public void testChangeStudentNameAndLastName() throws DataManagerException {
+    if (student != null){
+      String newName = "newName3";
+      String newLastName = "newLastName3";
+      
+      manager.changeStudentNameAndLastName(student, newName, newLastName);
+      
+      assertEquals(newName, student.getName());
+      assertEquals(newLastName, student.getLastName());
+    }
+  }
+
+  public void testChangeCourseTitleAndCoeff() throws DataManagerException {
+    if (course != null){
+      String newTitle = "newTitle";
+      float newCoeff = 99f;
+      
+      manager.changeCourseTitleAndCoeff(course, newTitle, newCoeff);
+      
+      assertEquals(newTitle, course.getTitle());
+      assertEquals(newCoeff == course.getCoeff(), true);
+    }
+  }
+
+  public void testChangeCourseTitle() throws DataManagerException {
+    if (course != null){
+      String newTitle = "newTitle2";
+      
+      manager.changeCourseTitle(course, newTitle);
+      
+      assertEquals(newTitle, course.getTitle());
+    }
+  }
+
+  public void testChangeCourseCoeff() throws DataManagerException {
+    if (course != null){
+      float newCoeff = 999f;
+      
+      manager.changeCourseCoeff(course, newCoeff);
+      
+      assertEquals(true, newCoeff == course.getCoeff());
+    }
   }
   
-  public void testRemoveAllStudentMarksForMark() {
-  }
+  public void testRemoveStudent() throws SQLException, DataManagerException {
+    int studentCount = manager.sizeOfTable(TABLE_STUDENT);
+    int testsCount = manager.sizeOfTable(TABLE_TEST);
+    int studentMarksCount = manager.sizeOfTable(TABLE_HAS_MARK);
+    
+    if (student != null) {
+      manager.removeStudent(student);
 
-  public void testChangeStudentMarkValue() {
+      assertEquals(studentCount - 1, manager.sizeOfTable(TABLE_STUDENT));
+      assertEquals(studentMarksCount - testsCount, manager
+          .sizeOfTable(TABLE_HAS_MARK));
+    }
   }
-
-  public void testChangeMarkDescription() {
+  
+  public void testRemoveMark() throws SQLException, DataManagerException {
+    int testsCount = manager.sizeOfTable(TABLE_TEST);
+    int studentMarksCount = manager.sizeOfTable(TABLE_HAS_MARK);
+    int studentCount = manager.sizeOfTable(TABLE_STUDENT);
+    
+    if (mark != null){
+      manager.removeMark(mark);
+      
+      assertEquals(testsCount - 1, manager.sizeOfTable(TABLE_TEST));
+      assertEquals(studentMarksCount - studentCount, manager.sizeOfTable(TABLE_HAS_MARK));
+    }
   }
-
+  
+  public void testRemoveCourse() throws DataManagerException, SQLException {
+    int n = manager.sizeOfTable(TABLE_COURSE);
+    
+    if (course != null){
+      manager.removeCourse(course);
+      
+      assertEquals(n -1, manager.sizeOfTable(TABLE_COURSE));
+    }
+  }
 }
